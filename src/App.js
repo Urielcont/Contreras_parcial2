1ce78ff23d48d2d9450570bc3d03a110953c77f5
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2025-07-03 14:48:49 -0600

    Cambios para examen parcial 2 Jose Uriel Contreras Sosa

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import YoImage from './yo.jpeg'; 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../yo.jpeg" className="App-logo" alt="logo" />
+        <p>
+          José Uriel Contreras Arevalos- EXAMEN PARCIAL 2
+        </p>
+        <a
+          className="App-link"
+          href="https://utd.edu.mx/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          UNIVERSIDAD TECNOLÓGICA DE DURANGO
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
